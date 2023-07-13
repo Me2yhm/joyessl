3f8cc8d92d538b784873bde03d53350b3e0ff7a0
Friede14dnnb <135705222+Friede14dnnb@users.noreply.github.com>
2023-07-13 16:25:25 +0800

    Update docker_to_data.py
    加了行注释

--- docker_to_data.py
+++ docker_to_data.py
@@ -1,5 +1,6 @@
 #使用命令行，将docker中固定位置的文件拉取到本地服务器
+#要在root账户里运行
 import subprocess
 
-subprocess.getoutput("sudo docker cp 7f860eb94b4b:/etc/nginx/certificates/cert-ny5jx0l5nq7r7m6p/cert.key /etc/nginx/test/")
-subprocess.getoutput("sudo docker cp 7f860eb94b4b:/etc/nginx/certificates/cert-ny5jx0l5nq7r7m6p/fullchain.cer /etc/nginx/test/")
+subprocess.getoutput("docker cp 7f860eb94b4b:/etc/nginx/certificates/cert-ny5jx0l5nq7r7m6p/cert.key /etc/nginx/test/")
+subprocess.getoutput("docker cp 7f860eb94b4b:/etc/nginx/certificates/cert-ny5jx0l5nq7r7m6p/fullchain.cer /etc/nginx/test/")
