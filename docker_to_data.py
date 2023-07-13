15eaddf9a0535d78e1c60fd4c5517787793b5cea
Friede14dnnb <135705222+Friede14dnnb@users.noreply.github.com>
2023-07-13 14:30:05 +0800

    Update docker_to_data.py

--- docker_to_data.py
+++ docker_to_data.py
@@ -1,6 +1,4 @@
 #使用命令行，将docker中固定位置的文件拉取到本地服务器
 import subprocess
 
-subprocess.run('登录')
-subprocess.communicate(input = '密码')
-subprocess.run('docker run -d -p 80:80 -v /etc/nginx/cert-ny5jx015nq7r7m6p:/etc/nginx/cert-ny5jx015nq7r7m6p nginx')
+subprocess.run("sudo docker cp 7f860eb94b4b:/etc/nginx/certificates/cert-ny5jx0l5nq7r7m6p/cert.key /etc/nginx/test/")
